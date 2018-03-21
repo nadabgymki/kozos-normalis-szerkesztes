d1a2d95949ac9f0afc48fa9de9469cb576cd4947
nagy.daniel <nagy.daniel@3101-100>
2018-03-21 18:38:40 +0100

    Add project files.

---
+++ kozos-normalis-szerkesztes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kozos_normalis_szerkesztes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
