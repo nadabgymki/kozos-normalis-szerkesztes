9ff0127b2b7c4f6829a8a0e1f096bf5d3d5ea5a6
3101TAN-05 <3101TAN-05@3101-5>
2018-03-21 18:46:52 +0100

    Zsolti módosításai

--- kozos-normalis-szerkesztes/Program.cs
+++ kozos-normalis-szerkesztes/Program.cs
@@ -11,6 +11,12 @@ namespace kozos_normalis_szerkesztes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Világ");
+            szakzsolt();
+            Console.ReadKey();
+        }
+        public static void szakzsolt()
+        {
+            Console.WriteLine("Szakmári Zsolt");
         }
     }
 }
